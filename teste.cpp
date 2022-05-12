c9634459b087c289131fd4a2cba07009ab5dcf48
Renan Lima <renan.bio.2019@hotmail.com>
2022-05-12 11:57:38 -0300

    linguagem c++

---
+++ teste.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+   int valor;
+   
+   cout << "Qual o valor? ";
+   cin >> valor;
+   cout << "O valor digitado foi " << valor << endl;
+   return 0;
+}
