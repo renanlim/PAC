c666c0bac76b6f945916a7506e2deec0144a053c
Renan Lima <101148875+renanlim@users.noreply.github.com>
2022-05-19 18:27:45 -0300

    solução questão 6

--- Lista 1/Questão 6/auxilio_creche.c
+++ Lista 1/Questão 6/auxilio_creche.c
@@ -7,3 +7,11 @@ void auxilio_creche (int quantdep)
     valor = 50 * dependente;
     printf ("\nO valor do seu auxílio será de: %d reais por dependente", valor);
 }
+
+
+void auxilio_creche (int quantdep)
+{
+    float aux;
+    aux = quantdep * 50;
+    printf ("\nO valor do seu auxílio será de: %.2f reais por dependente", aux);
+}
