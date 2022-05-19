ff2002f0064e0a8ecdaad31fb755c1cdbb8253d3
Renan Lima <101148875+renanlim@users.noreply.github.com>
2022-05-19 18:21:18 -0300

    Solução prof questão 4

--- Lista 1/Questão 4/calcula_salario.c
+++ Lista 1/Questão 4/calcula_salario.c
@@ -8,3 +8,11 @@ int calcula_salario (int valorhoratrab, int quanthoratrab)
     printf ("\nMeu salário base é de: %d\n", totsalario);
     return totsalario;
 }   
+
+float calcula_salario_base (float valorhoratrab, float qtdhoras)
+{
+   float sal;
+   sal = valorhoratrab * qtdhras;
+   printf ("\nMeu salário base é de: %.2f\n", sal);
+   return sal;
+}
