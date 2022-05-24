11ef147e927960370ea938e40dd0db1b2d49726d
Renan Lima <101148875+renanlim@users.noreply.github.com>
2022-05-24 16:51:24 -0300

    Update bonus_servico.c

--- Lista 1/Questão 5/bonus_servico.c
+++ Lista 1/Questão 5/bonus_servico.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
   
-int calcula_bonus (int salariobase, int temposervico)
-{
-    int bonus = 0.02 * salariobase * temposervico;
-    
-    printf ("\nO bonus por tempo de serviço é de %d reais\n", bonus);
-    return bonus;
-}  
-
 float calcula_bonus(int tempo, float salbase)
 {
     float bonus;
