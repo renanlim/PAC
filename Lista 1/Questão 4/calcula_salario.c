ca3f017ce113d549677bbf948995cce4cd1f320a
Renan Lima <101148875+renanlim@users.noreply.github.com>
2022-05-24 16:51:41 -0300

    Update calcula_salario.c

--- Lista 1/Questão 4/calcula_salario.c
+++ Lista 1/Questão 4/calcula_salario.c
@@ -1,14 +1,5 @@
 #include <stdio.h>
 
-int calcula_salario (int valorhoratrab, int quanthoratrab)
-{
-    int hora, totsalario;
-    valorhoratrab = hora * 91;
-    totsalario = valorhoratrab * quanthoratrab;
-    printf ("\nMeu salário base é de: %d\n", totsalario);
-    return totsalario;
-}   
-
 float calcula_salario_base (float valorhoratrab, float qtdhoras)
 {
    float sal;
