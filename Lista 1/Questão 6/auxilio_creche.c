9a56932cd7403bf4cbaf42a573d1d28b15be280c
Renan Lima <101148875+renanlim@users.noreply.github.com>
2022-05-24 16:52:25 -0300

    Update auxilio_creche.c

--- Lista 1/Questão 6/auxilio_creche.c
+++ Lista 1/Questão 6/auxilio_creche.c
@@ -2,15 +2,6 @@
 
 void auxilio_creche (int quantdep)
 {
-    int valor, dependente;
-    dependente = quantdep;
-    valor = 50 * dependente;
-    printf ("\nO valor do seu auxílio será de: %d reais por dependente", valor);
-}
-
-
-void auxilio_creche (int quantdep)
-{
     float aux;
     aux = quantdep * 50;
     printf ("\nO valor do seu auxílio será de: %.2f reais por dependente", aux);
