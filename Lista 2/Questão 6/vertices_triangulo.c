742ed8cff43002baa4d692745c66e01869237971
Renan Lima <101148875+renanlim@users.noreply.github.com>
2022-05-27 08:43:39 -0300

    Update vertices_triangulo.c

--- Lista 2/Questão 6/vertices_triangulo.c
+++ Lista 2/Questão 6/vertices_triangulo.c
@@ -3,10 +3,7 @@
 
 float calcula_distancia (float x1, float y1, float x2, float y2)
 {
-    float modulo;
-    modulo = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
-
-    return modulo;
+    return sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
 }
 int main ()
 {
