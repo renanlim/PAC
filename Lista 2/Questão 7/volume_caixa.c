f09cf9887e54601f36157977f4ac09dfa34b4a46
Renan Lima <101148875+renanlim@users.noreply.github.com>
2022-05-27 08:42:04 -0300

    Update volume_caixa.c

--- Lista 2/Questão 7/volume_caixa.c
+++ Lista 2/Questão 7/volume_caixa.c
@@ -4,32 +4,23 @@ float calcula_volume_caixa (float lA, float lB, float lC)
 { 
     return volume_caixa = lA * lB * lC;
 }
-int calcula_volume_cilindro (int h, int r)
+float calcula_volume_cilindro (float h, float r)
 {
     return volume_cilindro = 3.14 * h * r * 2;
 }
 int main ()
 {
-    float ladoA, ladoB, ladoC, h, r;
-    printf ("\nDimensão da caixa (lado a): ");
-    scanf ("%d", &ladoA);
-    printf ("\nDimensão da caixa (lado b): ");
-    scanf ("%d", &ladoB);
-    printf ("\nDimensão da caixa (lado c): ");
-    scanf ("%d", &ladoC);
+    float ladoA, ladoB, ladoC, h, r, volcilindro, caixa_com_furo, volcaixa;
+    printf ("\nDimensão da caixa (lado a, lado b, lado c): ");
+    scanf ("%f%f%f", &ladoA, &ladoB, &ladoC);
+    printf ("\nDimensão do cilindro (altura e raio): ");
+    scanf ("%f%f", &h, &r);
+    
+    volcilindro = calcula_volume_cilindro (h, r);
+    volcaixa = calcula_volume_caixa (ladoA, ladoB, ladoC);
+    caixa_com_furo = volcaixa - volcilindro;
 
-    int volcaixa = calcula_volume_caixa (ladoA, ladoB, ladoC);
-
-    printf ("\nDimensão do cilindro (altura): ");
-    scanf ("%d", &h);
-    printf ("\nDimensão do cilindro (raio): ");
-    scanf ("%d", &r);
-
-    int volcilindro = calcula_volume_cilindro (h, r);
-
-    int caixa_com_furo = volcaixa - volcilindro;
-
-    printf ("\nO volume da caixa com o furo cilindrico é de: %d\n", caixa_com_furo);
+    printf ("\nO volume da caixa com o furo cilindrico é de: %f\n", caixa_com_furo);
     
     return 0;
 }
