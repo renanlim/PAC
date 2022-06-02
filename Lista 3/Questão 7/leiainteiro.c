eeb0b766d2a9e23f5827e65b870c9a71402cee87
Renan Lima <101148875+renanlim@users.noreply.github.com>
2022-06-02 20:18:26 -0300

    Update leiainteiro.c

--- Lista 3/Questão 7/leiainteiro.c
+++ Lista 3/Questão 7/leiainteiro.c
@@ -1,31 +1,21 @@
 #include <stdio.h>
 
-int calculaMaiorNum (int num1, int num2, int num3)
+int calculaMaiorNum(int num1, int num2, int num3)
 {
     int maior = num1;
-
-    if (num2 > maior){
+    if(num2 > maior)
         maior = num2;
-    };
-
-    if(num3 > maior){
-        maior = num3;
-    }
-
+        if(num3 > maior)
+            maior = num3;
     return maior;
-
 }
-int main ()
+int main()
 {
     int n1, n2, n3, maiornum;
-    printf ("\nNumero 1: ");
-    scanf ("%d", &n1);
-    printf ("\nNumero 2: ");
-    scanf ("%d", &n2);
-    printf ("\nNumero 3: ");
-    scanf ("%d", &n3);
+    printf("\nDigite 3 numeros inteiros: ");
+    scanf("%d%d%d", &n1, &n2, &n3);
     maiornum = calculaMaiorNum (n1, n2, n3);
-    printf ("\nO maior numero é: %d\n", maiornum);
+    printf("\nO maior numero é: %d\n", maiornum);
 
     return 0; 
 }
