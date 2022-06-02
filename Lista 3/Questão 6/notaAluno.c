73c3cc0bf86baad37ac0965234783d53f0ec95f1
Renan Lima <101148875+renanlim@users.noreply.github.com>
2022-06-02 19:56:33 -0300

    Update notaAluno.c

--- Lista 3/Questão 6/notaAluno.c
+++ Lista 3/Questão 6/notaAluno.c
@@ -2,30 +2,28 @@
 
 void situacaoAluno(float media)
 {
-    if (media >= 5)
+    if(media >= 5)
     {
         printf ("\nAprovado");
-        if (media > 9)
-        {
-            printf ("\tParabéns\n");    
-        }
+        if(media > 9)
+           printf ("\tParabéns\n");    
     }
     else
-        if (media >= 3)
+        if(media >= 3)
             printf ("\nEm final\n");
         else
             printf ("\nReprovado\n");
 }
-int main ()
+int main()
 {
     int matr;
     float n1, n2, calculaMedia;
     printf("\nInforme matricula, Nota 1 e Nota 2: ");
-    scanf ("%d%f%f", &matr, &n1, &n2);
+    scanf("%d%f%f", &matr, &n1, &n2);
     calculaMedia = (n1 + n2)/2;
-    printf ("\nMatricula: %d", matr);
-    printf ("\nMedia: %.1f", calculaMedia);
-    situacaoAluno (calculaMedia);
+    printf("\nMatricula: %d", matr);
+    printf("\nMedia: %.1f", calculaMedia);
+    situacaoAluno(calculaMedia);
     
     return 0;
 }
