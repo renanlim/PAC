5e6435d31eeaae7201c4793514ee691d8f84a634
Renan Lima <101148875+renanlim@users.noreply.github.com>
2022-06-02 19:04:18 -0300

    Update areacorporal.c

--- Lista 3/Questão 4/areacorporal.c
+++ Lista 3/Questão 4/areacorporal.c
@@ -1,33 +1,28 @@
 #include <stdio.h>
 #include <math.h>
 
-float calculaArea ( float h, float p)
+float calculaArea(float h, float p)
 {
-    float A = sqrt(p*h)/60;
-    return A;
+    return sqrt(p*h)/60;
 }
-void alteracaoArea ( float area1, float area2)
+void alteracaoArea(float area1, float area2)
 {
-    if (area1 == area2)
-        printf ("\nManteve área inalterada\n");
+    if(area1 == area2)
+        printf("\nManteve área inalterada\n");
     else
-        if (area1 < area2)
-            printf ("\nAumentou a área corporal\n");
-            else
-                printf ("\nDiminui a área corporal\n");
+        if(area1 < area2)
+            printf("\nAumentou a área corporal\n");
+        else
+            printf("\nDiminui a área corporal\n");
 }
-int main ()
+int main()
 {
     float h, pinicial, pfinal, area1, area2;
-    printf ("\nInforme sua altura: ");
-    scanf ("%f", &h);
-    printf ("\nInforme seu peso no inicio do ano: ");
-    scanf ("%f", &pinicial);
-    printf ("\nInforme seu peso no final do ano: ");
-    scanf ("%f", &pfinal);
-
+    printf("\nInforme altura, peso no inicio do ano e peso no final do ano: ");
+    scanf("%f%f%f", &h, &pinicial, &pfinal);
     area1 = calculaArea (h, pinicial);
     area2 = calculaArea (h, pfinal);
 
-    alteracaoArea (area1, area2);
+    alteracaoArea(area1, area2);
+    return 0;
 }
