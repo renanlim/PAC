de9bd7d88847347446a14e6be97392dae964373e
Renan Lima <101148875+renanlim@users.noreply.github.com>
2022-06-02 19:52:31 -0300

    Update leiainteiro.c

--- Lista 3/Questão 7/leiainteiro.c
+++ Lista 3/Questão 7/leiainteiro.c
@@ -14,21 +14,6 @@ int calculaMaiorNum (int num1, int num2, int num3)
 
     return maior;
 
-/* 
- if (num1 > num2 && num1 > num3)
-    {
-        return num1;
-    }
-    else 
-        if (num2 > num1 && num2 > num3)
-        {
-            return num2;
-        }
-        else 
-            if (num3 > num1 && num3 > num2)
-            {
-                return num3;
-            } */
 }
 int main ()
 {
