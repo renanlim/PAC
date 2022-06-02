ab934f9dcfc22cff9f899110305a6101bafa8117
Renan Lima <101148875+renanlim@users.noreply.github.com>
2022-06-02 20:58:57 -0300

    Update qtdfunc.c

--- Lista 4/Questão 2/qtdfunc.c
+++ Lista 4/Questão 2/qtdfunc.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
 
-int funcionarias (int qtdfunc){
-    int matr, i, cont;
+int funcionarias(int qtdfunc){
+    int matr, i = 1, cont = 0;
     float sal;
     char sexo;
-    i = 1;
-    while (i <= qtdfunc){
+    while(i <= qtdfunc){
         printf ("Matricula, Sexo (M/F) e Salario: ");
         scanf ("%d %c%f", &matr, &sexo, &sal);
         i++;
-        if (sexo == 'F')
+        if(sexo == 'F')
             cont++;
     }
     return cont;
 }
-int main (){
+int main(){
     int qtd;
-    printf ("Quantidade de funcionarios: ");
-    scanf ("%d", &qtd);
-    printf ("Quantidade de funcionarias: %d\n", funcionarias(qtd));
-
+    printf("Quantidade de funcionarios: ");
+    scanf("%d", &qtd);
+    printf("Quantidade de funcionarias: %d\n", funcionarias(qtd));
     return 0;
 }
