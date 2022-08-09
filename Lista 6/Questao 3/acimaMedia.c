9e7f1dace3c17fcffb07fa4e4ccc9fd915ed1448
Renan Lima <101148875+renanlim@users.noreply.github.com>
2022-08-09 16:06:18 -0300

    Update acimaMedia.c

--- Lista 6/Questao 3/acimaMedia.c
+++ Lista 6/Questao 3/acimaMedia.c
@@ -1,4 +1,8 @@
-int acimaMeida(float v[], int qtd, float md){
+3) Faça uma função que receba como parâmetro um vetor de números reais, a quantidade de números
+armazenados no vetor e a média dos números. Esta função deverá retornar a quantidade de números acima
+da média dos números.
+    
+int acimaMedia(float v[], int qtd, float md){
     int acima, i;
     acima = 0;
     for (i = 0; i < qtd; i++){
