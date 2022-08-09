392881fb18ce34d39b3020969c23a9a8129f44c9
Renan Lima <101148875+renanlim@users.noreply.github.com>
2022-08-09 16:44:16 -0300

    Update situacao.c

--- Lista 5/Questao 2/situacao.c
+++ Lista 5/Questao 2/situacao.c
@@ -1,3 +1,11 @@
+2) Faça um programa, utilizando a função abaixo, para processar os 40 alunos de uma turma. Para cada aluno
+o programa deverá ler a matrícula e as duas notas, calcular a média e exibir a matrícula, a média e a situação
+do aluno (“aprovado”: média >= 5,0 ou “reprovado”: média < 5,0). Ao final o programa deverá exibir a
+quantidade de alunos aprovados e a quantidade de alunos reprovados da turma. Faça uma função que
+receba a média de um aluno, exiba a sua situação e contabilize este aluno nos aprovados ou nos reprovados.
+A função irá contabilizar os aprovados e os reprovados nas variáveis cujos endereços são fornecidos na
+chamada da função.
+
 #include <stdio.h>
 #define TOT 40
 
