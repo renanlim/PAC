fbd80e8a61c1230c6d3453bb49af0ce71114b128
Renan Lima <renan.bio.2019@hotmail.com>
2022-08-09 15:17:53 -0300

    lista 7

---
+++ Lista 7/q3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#define ALU 3
+#define MAXC 31
+struct aluno{
+    float mediaAluno;
+    char nome[MAXC];
+};
+typedef struct aluno Taluno;
+
+void exibe(Taluno v[], float mediaTurma){
+    int cont;
+    for(cont=0;cont<ALU;cont++){
+        printf("\nNome: %s", v[cont].nome);
+        printf("\tMedia aluno: %.1f", v[cont].mediaAluno);
+        if(v[cont].mediaAluno>mediaTurma)
+            printf("\tAcima da media da turma");
+        else
+            if(v[cont].mediaAluno==mediaTurma)
+                printf("\tNa media da turma");
+            else
+                printf("\tAbaixo da media da turma");
+    }
+}
+int main(){
+    Taluno aluno[MAXC];
+    float nota1, nota2, soma=0, md, mediaAluno, mediaTurma;
+    int cont;
+    for(cont=0;cont<ALU;cont++){
+        printf("Digite nome: ");
+        scanf(" %[^\n]", aluno[cont].nome);
+        printf("Digite as duas notas: ");
+        scanf("%f%f",&nota1, &nota2);
+        mediaAluno=(nota1+nota2)/2;
+        aluno[cont].mediaAluno=mediaAluno;
+        soma+=mediaAluno;
+        mediaTurma=soma/ALU;
+    }
+    exibe(aluno, mediaTurma);
+    return 0;
+}
---
+++ Lista 7/q5.c
@@ -0,0 +1,107 @@
+#include<stdio.h>
+#define TOT 3
+#define MAXC 21
+#define COL 4
+struct produto
+{
+	int cod;
+	char nome[MAXC];
+};
+typedef struct produto Tprod;
+void le_dados_produtos(Tprod v[], int mat[][COL])
+{
+	int i;
+	for(i = 0; i < TOT; i++)
+	{
+		printf("Digite codigo: ");
+		scanf("%d", &v[i].cod);
+		printf("Digite nome: ");
+		scanf(" %[^\n]", v[i].nome);
+		printf("Digite quantidade: ");
+		scanf("%d", &mat[i][COL - 1]);
+	}
+}
+void inicializa(int mat[][COL])
+{
+	int i, k;
+	for(i = 0; i  < TOT; i++)
+	{
+		for(k = 0; k < COL - 1; k++)
+		{
+			mat[i][k] = 0;
+		}
+	}
+}
+void gera_saida(Tprod v[], int mat[][COL])
+{
+	int i, k;
+	printf("\nCodigo\tNome\t\t\tSolicitada\tPendente\tDevolvida\tAtual");
+	for(i = 0; i  < TOT; i++)
+	{
+		printf("\n%d\t%-20s", v[i].cod, v[i].nome);
+		for(k = 0; k < COL; k++)
+		{
+			printf("\t%d", mat[i][k]);
+		}
+	}
+}
+int busca(Tprod v[], int elemproc)
+{
+	int i;
+	for(i = 0; i < TOT; i++)
+	{
+		if(v[i].cod == elemproc)
+			return i;
+	}
+	return -1;
+}
+int estoque_zerado(int mat[][COL])
+{
+	int i, zerado = 0;
+	for(i = 0; i < TOT; i++)
+		if(mat[i][3] == 0)
+			zerado++;
+	return zerado;
+}
+int main()
+{
+	Tprod v[TOT];
+	int mat[TOT][COL], codmov, qtdmov, pos, zerado;
+	char tipo;
+	inicializa(mat);
+	le_dados_produtos(v, mat);
+	printf("Digite codigo do produto (movimentacao): ");
+	scanf("%d", &codmov);
+	while (codmov != 0)
+	{
+		printf("Digite o tipo de operacao (s ou d): ");
+		scanf(" %c", &tipo);
+		printf("Digite quantidadde (movimentacao): ");
+		scanf("%d", &qtdmov);
+		pos = busca(v, codmov);
+		if(pos != -1)
+			if(tipo == 's')
+			{
+				mat[pos][0] += qtdmov;
+				if(mat[pos][3] >= qtdmov )
+					mat[pos][3] -= qtdmov;
+				else
+				{
+					mat[pos][2] += qtdmov - mat[pos][3];
+					mat[pos][3] = 0;
+				}
+			}	
+			else
+			{
+				mat[pos][2] += qtdmov;
+				mat[pos][3] += qtdmov;
+			}
+				
+		printf("Digite codigo do produto (movimentacao): ");
+		scanf("%d", &codmov);
+	}
+	gera_saida(v, mat);
+	zerado = estoque_zerado(mat);
+	printf("\nQuantidade produtos estoque zerado: %d", zerado);
+	return 0;
+}
