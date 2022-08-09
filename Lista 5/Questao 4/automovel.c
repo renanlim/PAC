182045231c01277a72240b710f2b2b2dcf02e447
Renan Lima <101148875+renanlim@users.noreply.github.com>
2022-08-09 16:46:08 -0300

    Update automovel.c

--- Lista 5/Questao 4/automovel.c
+++ Lista 5/Questao 4/automovel.c
@@ -1,3 +1,19 @@
+4) Uma loja que comercializa peças de automóvel possui as seguintes informações de cada peça: código e
+classificação da peça. As peças são classificadas de acordo com a tabela abaixo que não deverá ser lida:
+
+Classificacao Quantidade minima Quantidade maxima em estoque
+    1                  100                 120
+    2                  150                 180
+    3                  200                 250
+    4                  250                 300
+Faça um programa, utilizando as funções abaixo, para processar as 100 peças comercializadas. Para cada
+peça, o programa deverá ler os dados e exibir o seu código, a quantidade mínima e a quantidade máxima
+em estoque. Faça a seguinte função:
+a) le_valida_classificacao() → para ler, validar e retornar a classificação validada. A classificação é um
+número inteiro de 1 a 4.
+b) classe() → recebe como parâmetro a classificação da peça e guarda as quantidades mínima e máxima
+em estoque nas variáveis cujos endereços são fornecidos na chamada da função.
+
 #include <stdio.h>
 #define TOT 100
 
