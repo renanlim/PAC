65b800d0cbe738eba178b585354509634a4ea2fd
Renan Lima <101148875+renanlim@users.noreply.github.com>
2022-08-09 16:05:38 -0300

    Update parImpar.c

--- Lista 6/Questao 2/parImpar.c
+++ Lista 6/Questao 2/parImpar.c
@@ -1,3 +1,7 @@
+2) Faça uma função que receba como parâmetro um vetor de números inteiros e a quantidade de números
+armazenados no vetor. Esta função deverá calcular a quantidade de números pares e a quantidade de
+números ímpares, armazenando-as nas variáveis cujos endereços são fornecidos na chamada da função.
+    
 void parImpar(int *v, int qtd, int *par, int *impar){
     int cont;
     *par=*impar=0;
