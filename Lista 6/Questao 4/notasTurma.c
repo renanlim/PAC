da39d35504a05815d795553749548d672c98e7ef
Renan Lima <101148875+renanlim@users.noreply.github.com>
2022-08-09 16:07:49 -0300

    Update notasTurma.c

--- Lista 6/Questao 4/notasTurma.c
+++ Lista 6/Questao 4/notasTurma.c
@@ -1,3 +1,14 @@
+4) Faça um programa que leia as notas (número inteiro de 0 a 10) dos alunos de uma turma e exiba a
+quantidade de alunos em cada nota, conforme exemplo abaixo. Término da leitura: nota = -1. Faça as
+seguintes funções:
+a) inicializa(): recebe como parâmetro um vetor de números inteiros e inicializa-o apropriadamente.
+b) exibe():recebe como parâmetro um vetor de números inteiros já preenchido e exibe a quantidade de
+alunos em cada nota conforme exemplo abaixo.
+Nota 0: 2
+Nota 1: 4
+…
+Nota 10: 8
+    
 #include <stdio.h>
 #define TOT 11
 
