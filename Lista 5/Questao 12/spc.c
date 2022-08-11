5ea51fec9c10924bd5a3182f3529ca70a850f3fe
Renan Lima <renan.bio.2019@hotmail.com>
2022-08-11 12:03:37 -0300

    lista 5

--- Lista 5/Questao 12/spc.c
+++ Lista 5/Questao 12/spc.c
@@ -1,73 +1,90 @@
 #include <stdio.h>
-#define QTDP 5
-#define VENDAS 50
+#define SPC_MAX 1000
+#define VENDA_TOT 50
 
-struct inadimplentes{
+struct SPC
+{
     int cpf;
-    int qtd;
+    int qtdMeses;
 };
-typedef struct inadimplentes Tinadi;
 
-int leitura_SPC(Tinadi v[])
-{
-	int i, CPF, qtd;
-	for(i = 0; i < QTDP; i++)
-	{
-		printf("Digite CPF: ");
-        scanf("%d", &CPF);
-		printf("Digite quantidade de meses inadimplentes: ");
-        scanf("%d", &qtd);
-        v[i].cpf=CPF;
-	}
-    return qtd;
+typedef struct SPC TSpc;
+
+int leitura_SPC(TSpc v[]){
+    printf("inicio leitura_SPC\n");
+    int cpf;
+    int qtdSpc = 0;
+
+    printf("CPF. 0 para encerrar: ");
+    scanf("%d",&cpf);
+
+    for(int i=0; i<SPC_MAX; i++){
+        if(cpf == 0){
+            v[i].cpf= 0; 
+            v[i].qtdMeses= 0; 
+        }else{
+            v[i].cpf = cpf;
+            printf("Digite os meses inadimplentes:");
+            scanf("%d",&v[i].qtdMeses);
+
+            qtdSpc++;
+
+           printf("CPF. 0 para encerrar: ");
+            scanf("%d",&cpf);
+        }
+    };
+    return qtdSpc;
+    
 }
-int busca(Tinadi v[], int elemproc_CPF)
-{
-	int i;
-	for(i = 0; i < QTDP; i++)
-	{
-		if(v[i].cpf == elemproc_CPF)
-			return i; //posicao onde foi encontrado: cliente inadimplente
-	}
-	return -1; //caso não seja encontrado:  cliente não inadimplente
-}
 
-int main(){
-	Tinadi v[QTDP];
-	int pos, CPF, CPFCli, cod, qtdComprada, inadimplentes;
-    float venda=0, preco, total;
-	leitura_SPC(v);
-	printf("\nDigite CPF. 0 para encerrar: ");
-	scanf("%d", &CPF);
-	while (CPF != 0){
-		for(int cont=0;cont<VENDAS;cont++){
-            printf("\nDigite CPF do cliente: ");
-	        scanf("%d", &CPFCli);
-            pos=busca(v, CPFCli);
-
-            if(pos == -1){
-                printf("\nDigite Codigo do produto. 0 para encerrar: ");
-                scanf("%d", &cod);
-                while(cod!=0){
-                    printf("\nDigite Quantidade comprada: ");
-                    scanf("%d", &qtdComprada);
-                    printf("\nDigite Preco unitario: ");
-                    scanf("%f", &preco);
-                    printf("\nDigite Codigo do produto. 0 para encerrar: ");
-                    scanf("%d", &cod);
-                    venda++;
-                    total=qtdComprada*preco;
-                    printf("\nVenda %d: Valor total: %.1f", venda, total);
-                }
-            }
-             else
-                printf("\nVenda cancelada");
-                inadimplentes=leitura_SPC(v);
-                printf("\nCliente inadimplente a %d meses", inadimplentes);
-		printf("\nDigite CPF. 0 para encerrar: ");
-	    scanf("%d", &CPF);
-	    }
+int busca(TSpc v[], int cpf, int qtdInadiplentes){
+    printf("inicio busca SPC\n");
+    for (int i = 0; i < qtdInadiplentes; i++)
+    {
+        if(v[i].cpf == cpf){
+            return i;
+        };
     }
-	return 0;
+    return -1;
 }
 
+void main(){
+
+    TSpc spc[SPC_MAX];
+    int cpf, cod, preco, qtdProd;
+    float totalVenda;
+
+    int qtdInadiplentes = leitura_SPC(spc);
+
+    for(int i=0; i<VENDA_TOT; i++){
+        totalVenda = 0.0;
+        printf("Inicio venda %d\n", i+1);
+        printf("CPF:");
+        scanf("%d",&cpf);
+        int PosicaoInadiplente = busca(spc,cpf,qtdInadiplentes);
+
+        if(PosicaoInadiplente == -1){
+            //faz a venda porque nÃ£o achou o cliente na lista de spc
+            printf("Produto\n");
+            printf("COD. 0 para encerrar: ");
+            scanf("%d",&cod);
+            while(cod != 0){
+                printf("Digite PRECO:");
+                scanf("%d",&preco);
+                printf("Digite QTD comprada:");
+                scanf("%d",&qtdProd);
+
+                totalVenda += preco*qtdProd;
+
+                //Novo produto;
+                printf("COD. 0 para encerrar: ");
+                scanf("%d",&cod);
+            };
+            printf("Total da venda %0.2f\n", totalVenda);
+            printf("Fim venda %d\n", i+1);
+        }else{
+            printf("Venda Cancelada! Cliente Inadiplente a %d meses\n",spc[PosicaoInadiplente].qtdMeses);
+            printf("Fim venda %d\n", i+1);
+        }
+    }
+}
